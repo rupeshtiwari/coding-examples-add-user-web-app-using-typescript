5836a73a166b0e54b9b2b1f925826215cb648b9e
roopkt <roopkt@gmail.com>
2019-02-08 16:25:33 -0500

    adding webpack prod config

---
+++ webpack-prod.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ROOT = path.resolve(__dirname, 'src');
+const DESTINATION = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  context: ROOT,
+
+  entry: {
+    index: './index.ts',
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: DESTINATION,
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [ROOT, 'node_modules'],
+  },
+
+  module: {
+    rules: [
+      /****************
+       * PRE-LOADERS
+       *****************/
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'source-map-loader',
+      },
+      {
+        enforce: 'pre',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'tslint-loader',
+      },
+
+      /****************
+       * LOADERS
+       *****************/
+      //typescript
+      {
+        test: /\.ts$/,
+        exclude: [/node_modules/],
+        use: 'awesome-typescript-loader',
+      },
+      // css
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
+      // scss
+      {
+        test: /\.(scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      // images/fonts
+      {
+        test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: 'base64-inline-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      favicon: 'favicon.ico',
+      inject: 'body',
+    }),
+  ],
+
+  performance: { hints: false },
+
+  devtool: 'source-map',
+
+  devServer: {},
+};
